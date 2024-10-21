7cc1d222a75eaa48780e9730e6b17b09d15ebc19
Nicholas Doropoulos <ndoropoulos@yahoo.com>
2024-10-21 10:47:29 +0200

    Add error handling to user input

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# A simple BMI calculator
+# Initialise the variables
+height = None
+weight = None
 
+# Ask the user to supply their height in centimeters which is then
+# converted into a decimal number representing meters
+while True:
+    try:
+        height = float(input("What is your height in centimeters? "))
+        real_height = height / 100
+        break
+    except ValueError:
+        print("Please enter a valid value (i.e. 176): ")
 
-# The variables storing the user supplied height and weight
-height = float(input("What is your height? "))
-weight = int(input("What is your weight? "))
+# Ask the user to supply a whole number representing their weight        
+while True:
+    try:
+        weight = int(input("What is your weight in kilograms? "))
+        break
+    except ValueError:
+        print("Please enter a whole number (i.e. 80): ")
 
-# The formula calculating the user's BMI
-bmi = weight / height ** 2
+# Calculate the user's BMI while rounding the result
+# to 2 decimal places
+bmi = round((weight / real_height ** 2), 2)
 
-# Displaying to the user their calculated BMI
-print("Your BMI is " + " " + str(bmi))
+# Display to the user their BMI
+print(f'Since your height is {real_height}m and your weight is {weight}kg, your BMI is {bmi}.')
+
