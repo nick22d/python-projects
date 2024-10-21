23fa1a3340d0165a3d5d80fc622c74652652116f
Nicholas Doropoulos <ndoropoulos@yahoo.com>
2024-10-21 13:12:57 +0200

    Add comments

--- Tip_calculator/main.py
+++ Tip_calculator/main.py
@@ -1,8 +1,8 @@
-
-
-
+# Greet the user.
 print("Welcome to the tip calculator!")
 
+# Ask the user for their total bill in the form of
+# a decimal number and perform error handling.
 while True:
     try:
         total_bill = float(input("What was the total bill? $"))
@@ -10,6 +10,8 @@ while True:
     except ValueError:
         print("Please provide your total bill in decimal format (i.e. 150.36)")
 
+# Ask the user for their desired tip and ensure that it's either
+# 10 or 12 or 15. Also handle errors accordingly.
 while True:
     try:
         tip = int(input("How much tip would you like to give? 10, 12, or 15? ")) 
@@ -21,6 +23,8 @@ while True:
     except ValueError:
         print("Please choose a tip from the following values: 10, 12, 15: ")
 
+# Ask the user for the number of participants amongst 
+# whom the bill will be split.
 while True:
     try:
         people = int(input("How many people to split the bill? "))
@@ -28,7 +32,8 @@ while True:
     except ValueError:
         print("Please provide a whole number (i.e. 5, 7, 10)")
 
-
+# Calculate the individual bill that everybody will have to pay.
 individual_bill = round((total_bill + desired_tip) / people, 2)
 
+# Display to the user the individual bill.
 print(f"Each person should pay: ${individual_bill}")
