7c1d44390773ffeb30b90a4f7738c3c5741f590c
Nicholas Doropoulos <ndoropoulos@yahoo.com>
2024-10-22 20:32:42 +0200

    Convert variable names to uppercase in BMI project

--- BMI_calculator/main.py
+++ BMI_calculator/main.py
@@ -1,31 +1,31 @@
 """This is a program that calculates one's BMI."""
 
 # Initialise the variables
-height = None
-weight = None
+HEIGHT = None
+WEIGHT = None
 
-# Ask the user to supply their height in centimeters which is then
+# Ask the user to supply their HEIGHT in centimeters which is then
 # converted into a decimal number representing meters
 while True:
     try:
-        height = float(input("What is your height in centimeters? "))
-        real_height = height / 100
+        HEIGHT = float(input("What is your HEIGHT in centimeters? "))
+        real_HEIGHT = HEIGHT / 100
         break
     except ValueError:
         print("Please enter a valid value (i.e. 176): ")
 
-# Ask the user to supply a whole number representing their weight
+# Ask the user to supply a whole number representing their WEIGHT
 while True:
     try:
-        weight = int(input("What is your weight in kilograms? "))
+        WEIGHT = int(input("What is your WEIGHT in kilograms? "))
         break
     except ValueError:
         print("Please enter a whole number (i.e. 80): ")
 
 # Calculate the user's BMI while rounding the result
 # to 2 decimal places
-bmi = round((weight / real_height ** 2), 2)
+bmi = round((WEIGHT / real_HEIGHT ** 2), 2)
 
 # Display to the user their BMI
-print(f"Since your height is {real_height}m and your weight is {weight}kg, your BMI is {bmi}.")
+print(f"Since your HEIGHT is {real_HEIGHT}m and your WEIGHT is {WEIGHT}kg, your BMI is {bmi}.")
 
