b163d9d5f690edb9412ddbbf9d4887d48a291b6d
Nicholas Doropoulos <ndoropoulos@yahoo.com>
2024-10-27 13:34:28 +0100

    Add comments to code for rock,paper,scissors game

--- Rock_paper_scissors/main.py
+++ Rock_paper_scissors/main.py
@@ -15,22 +15,31 @@ print('''       _                                         _
                     |_|         |_|                                                
 ''')
 
+# Create a list of the imported variables 
 CHOICES = [variables.ROCK, variables.PAPER, variables.SCISSORS]
+
+# Define variables for the possible outcomes
 WIN_MESSAGE = "You win."
 LOSE_MESSAGE = "You lose."
 DRAW_MESSAGE = "It's a draw."
 
+# Create a dictionary to store the results and 
+# map the user's input directly to the imported variables
 OUTCOMES = {
     (0, 0): DRAW_MESSAGE, (0, 1): LOSE_MESSAGE, (0, 2): WIN_MESSAGE,
     (1, 0): WIN_MESSAGE, (1, 1): DRAW_MESSAGE, (1, 2): LOSE_MESSAGE,
     (2, 0): LOSE_MESSAGE, (2, 1): WIN_MESSAGE, (2, 2): DRAW_MESSAGE,
 }
 
+# Encapsulate the code in a while loop 
+# to perform error handling
 while True:
     try:
+        # Define the variable that will store the computer's choice
         COMPUTER_INPUT = random.choice(CHOICES)
+        # Define the variable that will store the user's choice
         USER_INPUT = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
-
+        # Force the user to select 0, 1 or 2 with a conditional statement
         if USER_INPUT not in (0, 1, 2):
             print("Please enter 0 for Rock, 1 for Paper or 2 for Scissors: ")
             continue
