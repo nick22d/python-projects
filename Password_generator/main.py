24363c9246b56a20366a1bbd06723c4de5cebd23
Nicholas Doropoulos <ndoropoulos@yahoo.com>
2024-10-28 16:08:41 +0100

    Improve the printed result and add comments to the code

--- Password_generator/main.py
+++ Password_generator/main.py
@@ -1,28 +1,38 @@
 """This is a password generator"""
 
+# Import the random and string modules
 import string
 import random
 
+# Define the project's variables
 alphabet = list(string.ascii_lowercase)
 special_symbols = ['!', '"', '$', '^']
 numbers = list(string.digits)
 password = []
 
+# Greet the user
 print("Welcome to the password generator!")
 
+# Define the variables that will store the user's choices
 nr_letters = int(input("How many letters would you like in your password? "))
 nr_symbols = int(input("How many symbols would you like? "))
 nr_numbers = int(input("How many numbers would you like? "))
 
+# Populate the password list with the number of
+# letters chosen
 for letter in range(nr_letters):
     password.append(random.choice(alphabet))
 
+# Populate the password list with the number of
+# symbols chosen
 for symbol in range(nr_symbols):
     password.append(random.choice(special_symbols))
 
+# Populate the password list with the number of 
+# numbers chosen
 for number in range(nr_numbers):
     password.append(random.choice(numbers))
 
 password = random.sample(password, len(password))
 
-print("".join(password))
+print(f"Your password is: {''.join(password)}")
